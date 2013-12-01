23da464e9d14fdf02cc81a391d7e2b59ae8d2c13
Courtney Nguyen <3Solstice@gmail.com>
2013-12-01 11:01:19 -0700

    injecting html works

--- public/static/script.js
+++ public/static/script.js
@@ -1,27 +1,23 @@
-// script.js
-
-	// create the module and name it scotchApp
 	var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-		// configure our routes
 	scotchApp.config(function($routeProvider) {
 		$routeProvider
 
 			// route for the home page
 			.when('/', {
-				templateUrl : 'pages/home.html',
+				templateUrl : 'home.html',
 				controller  : 'mainController'
 			})
 
 			// route for the about page
 			.when('/about', {
-				templateUrl : 'pages/about.html',
+				templateUrl : 'about.html',
 				controller  : 'aboutController'
 			})
 
 			// route for the contact page
 			.when('/contact', {
-				templateUrl : 'pages/contact.html',
+				templateUrl : 'contact.html',
 				controller  : 'contactController'
 			});
 	});
@@ -39,3 +35,6 @@
 	scotchApp.controller('contactController', function($scope) {
 		$scope.message = 'Contact us! JK. This is just a demo.';
 	});
+
+
+
