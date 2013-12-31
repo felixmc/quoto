8ea9cc94a033972ef8c0b80b19e4de7b2273d431
Courtney Nguyen <3Solstice@gmail.com>
2013-12-30 19:24:08 -0700

    Added some styling and fixed bootstrap

--- public/static/script.js
+++ public/static/script.js
@@ -25,7 +25,7 @@
 	// create the controller and inject Angular's $scope
 	scotchApp.controller('mainController', function($scope) {
 		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
+		$scope.message = 'Home page!';
 	});
 
 	scotchApp.controller('aboutController', function($scope) {
