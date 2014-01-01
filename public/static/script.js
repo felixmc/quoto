0cc350e6a7d023092a8b50bddc4015c3b43c00c7
Courtney Nguyen <3Solstice@gmail.com>
2014-01-01 13:42:59 -0700

    Added some asthetics

--- public/static/script.js
+++ public/static/script.js
@@ -1,39 +1,32 @@
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	var quotoApp = angular.module('quotoApp', ['ngRoute']);
 
-	scotchApp.config(function($routeProvider) {
+	quotoApp.config(function($routeProvider) {
 		$routeProvider
-
-			// route for the home page
 			.when('/', {
 				templateUrl : 'home.html',
 				controller  : 'mainController'
 			})
-
-			// route for the about page
 			.when('/about', {
 				templateUrl : 'about.html',
 				controller  : 'aboutController'
 			})
-
-			// route for the contact page
 			.when('/contact', {
-				templateUrl : 'contact.html',
-				controller  : 'contactController'
+				templateUrl : 'search-quotes.html',
+				controller  : 'searchController'
 			});
 	});
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Home page!';
+	quotoApp.controller('mainController', function($scope) {
+		$scope.message = 'Welcome!';
 	});
 
-	scotchApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
+	quotoApp.controller('aboutController', function($scope) {
+		$scope.message = 'About Us';
 	});
 
-	scotchApp.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
+	quotoApp.controller('searchController', function($scope) {
+		$scope.message = 'Search for...';
 	});
 
 
